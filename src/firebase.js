a8dec98b6952e820d4736bc6e8c1be36bab99d50
Caroline Boyle <carolinerboyle@gmail.com>
2023-03-16 14:55:15 -0400

    env setup

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,15 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+require("dotenv").config();
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDxGbHCoFToaRmAwCZ8wJyT0pYUd3tNBz8",
+  apiKey: process.env.API_KEY,
   authDomain: "yummify-352b4.firebaseapp.com",
   databaseURL: "https://yummify-352b4-default-rtdb.firebaseio.com",
   projectId: "yummify-352b4",
