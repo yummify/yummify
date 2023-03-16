122158df78fc083d034213b8d7cd1a410657c42f
Monique Hayes <moniqueehayes5@gmail.com>
2023-03-16 13:53:25 -0500

    privacy

--- src/firebase.js
+++ src/firebase.js
@@ -2,20 +2,23 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+require('dotenv').config();
+
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDxGbHCoFToaRmAwCZ8wJyT0pYUd3tNBz8",
-  authDomain: "yummify-352b4.firebaseapp.com",
-  databaseURL: "https://yummify-352b4-default-rtdb.firebaseio.com",
-  projectId: "yummify-352b4",
-  storageBucket: "yummify-352b4.appspot.com",
-  messagingSenderId: "61047984799",
-  appId: "1:61047984799:web:e11a33049db5af613fc8aa",
-  measurementId: "G-ZSQQSCGKRV",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 
 // Initialize Firebase
