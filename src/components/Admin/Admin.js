32f69c11ea7bdf3076f68be686fbbee307768acd
Monique Hayes <moniqueehayes5@gmail.com>
2023-03-20 15:43:03 -0500

    created admin component file & route

---
+++ src/components/Admin/Admin.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const AdminHome = () => {
+
+    return (
+        <>
+        <h2>Admin Dashboard</h2>
+        </>
+    )
+    // OPTIONS: 
+     // - manage users
+     // - manage restaurants
+     // view home page
+
+     //ADD CONDITIONAL LOGIC TO THE LOGIN FUNCTION -> IF USER IS ADMIN, NAVIGATE TO THIS HOME PAGE INSTEAD
+
+};
+
+export default AdminHome;
