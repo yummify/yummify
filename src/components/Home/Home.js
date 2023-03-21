5cb88151a402f8ed68ae08154f649d5993b29716
msiva06 <m.sivasubramanian06@gmail.com>
2023-03-20 22:35:17 -0400

    basic styling done in user profile page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,21 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Button } from "react-bootstrap";
+import { Button, Card, Col, Image, Container, Row } from "react-bootstrap";
 import { signOut } from "firebase/auth";
 import { auth } from "../../firebase/config";
 import { useNavigate } from "react-router-dom";
 import { fetchUserAsync, selectUser } from "../User/userSlice";
 import { selectAuth } from "../Auth/authSlice";
-//import { useAuth } from "../../contexts/AuthContext";
+import { useAuth } from "../../contexts/AuthContext";
 
 const Home = () => {
   const authUser = useSelector(selectAuth);
-  const user = useSelector(selectUser);
-  console.log("User:", user);
+  //const user = useSelector(selectUser);
+  //console.log("User:", user);
   console.log("AuthUser id:", authUser.userId);
   //const [loading, setLoading] = useState(true);
   //console.log("Auth User:", auth.currentUser.uid);
-  // const { fetchUser, user } = useAuth();
+  const { user } = useAuth();
+  console.log("User:", user);
   // console.log("User from auth context:", user);
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -36,8 +37,40 @@ const Home = () => {
     <div>
       {user?.userId && (
         <div>
-          <h1>Welcome {user?.name}</h1>
-          <p>Email :{user?.email}</p>
+          <Container>
+            <Row>
+              <div className="d-flex text-black">
+                <Col>
+                  <div
+                    className="flex-shrink-0 d-flex flex-column"
+                    style={{ marginTop: "5rem", marginLeft: "1rem" }}
+                  >
+                    <Image
+                      fluid
+                      src={user.image}
+                      alt="image of user"
+                      thumbnail
+                      style={{ width: "100px", borderRadius: "10px" }}
+                    />
+                    <Button style={{ padding: "0.2rem", marginTop: "1rem" }}>
+                      Upload Photo
+                    </Button>
+                  </div>
+                </Col>
+                <Col>
+                  <div
+                    className="flex-grow-1 ms-3"
+                    style={{ marginTop: "5rem" }}
+                  >
+                    <h1>{user?.name}</h1>
+                    <p>Email :{user?.email}</p>
+                    <p>PhoneNumber:{user?.phoneNumber}</p>
+                    <p>Zipcode:{user.zipcode}</p>
+                  </div>
+                </Col>
+              </div>
+            </Row>
+          </Container>
         </div>
       )}
       <Button onClick={logout}>Logout</Button>
