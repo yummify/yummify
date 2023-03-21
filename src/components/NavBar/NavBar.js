3846c828ae8beb37ab8e36a31f9008ca44cb402d
Monique Hayes <moniqueehayes5@gmail.com>
2023-03-21 16:20:47 -0500

    basic navbar component made with diff versions for admin and customer. still need to add restaurant owner version

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { useSelector } from "react-redux";
+
+
+const NavBar = () => {
+    const user = useSelector(state => state.authenticate);
+
+    //VS RESTAURANT - how to distinguish???
+
+    return (
+        <NavBar>
+            <Container>
+                    {user.isAdmin ? 
+                        <Nav>
+                        <Nav.Link href='/admin'>Dashboard</Nav.Link>
+                        <Nav.Link href='/admin/manage-users'>Manage Users</Nav.Link>
+                        <Nav.Link href='/admin/manage-restaurants'>Manage Restaurants</Nav.Link>
+                        <Nav.Link href='/admin/order-history'>Order History</Nav.Link>
+                        <Nav.Link href='/'>Customer View</Nav.Link>
+                        </Nav>
+                    : 
+                    <Nav>
+                    <Nav.Link href='/'>Home</Nav.Link>
+                    <Nav.Link href='/map'>Map View</Nav.Link>
+                    <Nav.Link href='/profile'>Profile</Nav.Link>
+                    <Nav.Link href='/cart'>Cart</Nav.Link>
+                    </Nav>}
+            </Container>
+        </NavBar>
+    )
+};
+
+export default NavBar;
