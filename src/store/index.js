02966628fe5d20510120a4110dc3feeac6f4a92b
msiva06 <m.sivasubramanian06@gmail.com>
2023-03-22 14:24:14 -0400

    added restaurant slice in the reducer

--- src/store/index.js
+++ src/store/index.js
@@ -2,15 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "../components/Auth/authSlice";
 import userSlice from "../components/User/userSlice";
 import usersSlice from "../components/Users/usersSlice";
-import  restaurantsSlice  from "../components/AllRestaurants/allRestaurantsSlice";
+import restaurantsSlice from "../components/AllRestaurants/allRestaurantsSlice";
+import restaurantSlice from "../components/Restaurant/restaurantSlice";
 export const store = configureStore({
   reducer: {
     authenticate: authSlice,
     user: userSlice,
     users: usersSlice,
-    restaurants: restaurantsSlice
+    restaurants: restaurantsSlice,
+    restaurant: restaurantSlice,
   },
 });
 export default store;
 export * from "../components/Auth/authSlice";
-
