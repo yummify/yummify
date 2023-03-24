fe8f36215173b6458f0e2bc6a1b5a7049554a6a4
msiva06 <m.sivasubramanian06@gmail.com>
2023-03-23 22:44:00 -0400

    modified private route to use auth.currentuser

--- src/components/Auth/PrivateRestaurantRoute.js
+++ src/components/Auth/PrivateRestaurantRoute.js
@@ -1,29 +1,17 @@
 import React from "react";
 import { Route, redirect, Navigate } from "react-router-dom";
 import { useAuthRes } from "../../contexts/AuthResContext";
+import { auth } from "../../firebase/config";
 
 export default function PrivateRestaurantRoute({
   //   component: Component,
   //   ...rest
   children,
 }) {
-  const { restaurant } = useAuthRes();
-  console.log("Restaurant in Private router:", restaurant);
-  if (!restaurant) {
+  //const { restaurant } = useAuthRes();
+  //console.log("Restaurant in Private router:", restaurant);
+  if (!auth?.currentUser) {
     return <Navigate to="/restaurantstart" replace />;
   }
   return children;
-  //return (
-  // <Route
-  //   {...rest}
-  //   render={(props) => {
-  //     return restaurant ? (
-  //       <Component {...props} />
-  //     ) : (
-  //       redirect("/restaurantstart")
-  //     );
-  //   }}
-  // ></Route>
-
-  //);
 }
--- src/components/Auth/PrivateUserRoute.js
+++ src/components/Auth/PrivateUserRoute.js
@@ -1,29 +1,18 @@
 import React from "react";
 import { Route, redirect, Navigate } from "react-router-dom";
 import { useAuth } from "../../contexts/AuthContext";
+import { auth } from "../../firebase/config";
 
 export default function PrivateUserRoute({
   //   component: Component,
   //   ...rest
   children,
 }) {
-  const { user } = useAuth();
-  console.log("User in Private router:", user);
-  if (!user) {
+  //const { user } = useAuth();
+  //console.log("User in Private router:", user);
+  //if (!user) {
+  if (!auth?.currentUser) {
     return <Navigate to="/userstart" replace />;
   }
   return children;
-  //return (
-  // <Route
-  //   {...rest}
-  //   render={(props) => {
-  //     return restaurant ? (
-  //       <Component {...props} />
-  //     ) : (
-  //       redirect("/restaurantstart")
-  //     );
-  //   }}
-  // ></Route>
-
-  //);
 }
