3b4fc03275c3a0e4e47f8f3761b334a0b0de3767
msiva06 <m.sivasubramanian06@gmail.com>
2023-03-23 22:46:28 -0400

    updated user profile

--- src/components/User/EditUserProfile.js
+++ src/components/User/EditUserProfile.js
@@ -1,33 +1,57 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Form, Button } from "react-bootstrap";
 import { addUserAsync } from "./userSlice";
 import { useDispatch, useSelector } from "react-redux";
-import { selectAuth } from "../Auth/authSlice";
+import { fetchUserAsync, editUserAsync, selectUser } from "./userSlice";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthContext";
 
 const EditUserProfile = () => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [zipcode, setZipcode] = useState("");
+  const [email, setEmail] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const { user } = useAuth();
+  const authuser = useSelector(selectUser);
+  const name = authuser?.name?.split(" ");
+
+  useEffect(() => {
+    if (user?.userId) dispatch(fetchUserAsync(user?.userId));
+  }, [user?.userId]);
+
   //console.log("auth.currentuser+++++++" + auth.currentUser.email);
-  const authenticate = useSelector(selectAuth);
-  console.log("Authen:", authenticate);
-  const createNewUser = () => {
+  // const authenticate = useSelector(selectAuth);
+  // console.log("Authen:", authenticate);
+  // const createNewUser = () => {
+  //   const reqbody = {
+  //     userId: authenticate.userId,
+  //     name: authenticate.displayName
+  //       ? authenticate.displayName
+  //       : firstName + " " + lastName,
+  //     email: authenticate.email,
+  //     phoneNumber: authenticate.phoneNumber
+  //       ? authenticate.phoneNumber
+  //       : phoneNumber,
+  //     zipcode: zipcode,
+  //   };
+  //   dispatch(addUserAsync(reqbody)).then(() => navigate("/home"));
+  // };
+
+  const editUser = () => {
+    const fName =
+      firstName !== undefined && firstName !== "" ? firstName : name[0];
+    const lName =
+      lastName !== undefined && lastName !== "" ? lastName : name[1];
     const reqbody = {
-      userId: authenticate.userId,
-      name: authenticate.displayName
-        ? authenticate.displayName
-        : firstName + " " + lastName,
-      email: authenticate.email,
-      phoneNumber: authenticate.phoneNumber
-        ? authenticate.phoneNumber
-        : phoneNumber,
-      zipcode: zipcode,
+      userId: user.userId,
+      name: fName + " " + lName,
+      phoneNumber: phoneNumber ? phoneNumber : authuser.phoneNumber,
+      zipcode: zipcode ? zipcode : authuser.zipcode,
     };
-    dispatch(addUserAsync(reqbody)).then(() => navigate("/home"));
+    dispatch(editUserAsync(reqbody)).then(() => navigate("/userprofile"));
   };
 
   return (
@@ -38,6 +62,7 @@ const EditUserProfile = () => {
           <Form.Label>FirstName :</Form.Label>
           <Form.Control
             type="text"
+            placeholder={name[0]}
             onChange={(event) => setFirstName(event.target.value)}
           />
         </Form.Group>
@@ -45,6 +70,7 @@ const EditUserProfile = () => {
           <Form.Label>LastName :</Form.Label>
           <Form.Control
             type="text"
+            placeholder={name[1]}
             onChange={(event) => setLastName(event.target.value)}
           />
         </Form.Group>
@@ -52,6 +78,7 @@ const EditUserProfile = () => {
           <Form.Label>PhoneNumber :</Form.Label>
           <Form.Control
             type="text"
+            placeholder={authuser.phoneNumber}
             onChange={(event) => setPhoneNumber(event.target.value)}
           />
         </Form.Group>
@@ -59,11 +86,13 @@ const EditUserProfile = () => {
           <Form.Label>Zipcode :</Form.Label>
           <Form.Control
             type="text"
+            placeholder={authuser.zipcode}
             onChange={(event) => setZipcode(event.target.value)}
           />
         </Form.Group>
 
-        <Button onClick={createNewUser}>Next</Button>
+        <Button onClick={editUser}>Save</Button>
+        <Button onClick={() => navigate("/userprofile")}>Cancel</Button>
       </Form>
     </div>
   );
