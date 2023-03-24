abd39ee85bbc7f39fd725edddd6613879b8bb055
msiva06 <m.sivasubramanian06@gmail.com>
2023-03-23 22:39:00 -0400

    cleaned commented code

--- src/index.js
+++ src/index.js
@@ -1,27 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
-import Login from "./components/Auth/Login";
-import UserSignUp from "./components/Auth/UserSignUp";
-import Home from "./components/Home/Home";
-import EditUserProfile from "./components/User/EditUserProfile";
-import Users from "./components/Users/Users";
 
 import { BrowserRouter as Router } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import store from "./store";
 import { Provider } from "react-redux";
-import { AuthProvider } from "./contexts/AuthContext";
-import RestaurantSignUp from "./components/Auth/RestaurantSignUp";
-import RestaurantProfile from "./components/Restaurant/RestaurantProfile";
-import { AuthResProvider } from "./contexts/AuthResContext";
-import RestaurantLogin from "./components/Auth/RestaurantLogin";
-import RestaurantStartPage from "./components/App/RestaurantStartPage";
-import UserStartPage from "./components/App/UserStartPage";
-//import { AuthProvider } from "./contexts/AuthContext";
-import Bag from "./components/Bag/Bag";
-import AddBagForm from "./components/Bag/AddBagForm";
-import EditBagForm from "./components/Bag/EditBagForm";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -31,32 +15,5 @@ root.render(
         <Route path="/*" element={<App />} />
       </Routes>
     </Router>
-    {/* <Router>
-      <Routes></Routes>
-    </Router>
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/userstart" element={<UserStartPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/usersignup" element={<UserSignUp />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/edituserprofile" element={<EditUserProfile />} />
-          <Route path="/users" element={<Users />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
-    <AuthResProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/restaurantstart" element={<RestaurantStartPage />} />
-          <Route path="/loginrestaurant" element={<RestaurantLogin />} />
-          <Route path="/restaurantsignup" element={<RestaurantSignUp />} />
-          <Route path="/restaurantprofile" element={<RestaurantProfile />} />
-        </Routes>
-      </Router>
-    </AuthResProvider> */}
   </Provider>
 );
