2da7db517a06a246a55dae0de2cd88ae28931a8f
msiva06 <m.sivasubramanian06@gmail.com>
2023-03-24 11:58:09 -0400

    private admin route added

---
+++ src/components/Auth/PrivateAdminRoute.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthContext";
+
+export default function PrivateAdminRoute({ children }) {
+  const { user } = useAuth();
+  console.log("User in Admin router:", user);
+  if (!user || !user?.isAdmin || user?.isRestaurantOwner) {
+    return <Navigate to="/userstart" replace />;
+  }
+  return children;
+}
