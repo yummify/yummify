aa9b088dccd155ce73179652cd190f71dca275d2
msiva06 <m.sivasubramanian06@gmail.com>
2023-03-23 22:43:01 -0400

    included user login and signup buttons

--- src/components/App/UserStartPage.js
+++ src/components/App/UserStartPage.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import { Button } from "react-bootstrap";
+import { changeForm } from "./appSlice";
 function UserStartPage() {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
       <Button onClick={() => navigate("/login")}>Login</Button>
