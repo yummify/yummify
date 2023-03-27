e258f5f7952c8aae0fed7037b68a5e15d4282206
msiva06 <m.sivasubramanian06@gmail.com>
2023-03-26 21:51:52 -0400

    added navbar headers according to auth

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,17 +1,61 @@
 import React from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
-import { useSelector } from "react-redux";
-
+import { Navbar, Nav, Container, Button } from "react-bootstrap";
+import { useAuth } from "../../contexts/AuthContext";
+import { signOut } from "firebase/auth";
+import { auth } from "../../firebase/config";
+import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
-    const user = useSelector(state => state.authenticate);
-    
-    //VS RESTAURANT - how to distinguish???
+  const authUser = useAuth();
+  const navigate = useNavigate();
+  console.log("AuthUser in Navbar:", authUser?.user);
 
-    return (
-        <Navbar>
-            <Container>
-                    {user.isAdmin ? 
+  //VS RESTAURANT - how to distinguish???
+
+  const logout = async () => {
+    try {
+      await signOut(auth);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <Navbar>
+      <Container>
+        {!authUser?.user?.userId ? (
+          <Nav>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/userstart">User</Nav.Link>
+            <Nav.Link href="/restaurantstart">Restaurant</Nav.Link>
+          </Nav>
+        ) : authUser?.user?.isRestaurantOwner ? (
+          <Nav>
+            <Nav.Link href="/restaurantprofile">Profile</Nav.Link>
+            <Nav.Link href="/">Store</Nav.Link>
+            <Nav.Link href="/">Orders</Nav.Link>
+            <Button onClick={logout}>Logout</Button>
+          </Nav>
+        ) : authUser?.user?.isAdmin ? (
+          <Nav>
+            <Nav.Link href="/adminprofile">Profile</Nav.Link>
+            <Nav.Link href="/admin/manage-users">Manage Users</Nav.Link>
+            <Nav.Link href="/admin/manage-restaurants">
+              Manage Restaurants
+            </Nav.Link>
+            <Nav.Link href="/admin/order-history">Order History</Nav.Link>
+            <Button onClick={logout}>Logout</Button>
+          </Nav>
+        ) : (
+          <Nav>
+            <Nav.Link href="/userprofile">Profile</Nav.Link>
+            <Nav.Link href="/map">Map View</Nav.Link>
+            <Nav.Link href="/">Cart</Nav.Link>
+            <Button onClick={logout}>Logout</Button>
+          </Nav>
+        )}
+        {/* {user.isAdmin ? 
                         <Nav>
                         <Nav.Link href='/admin'>Dashboard</Nav.Link>
                         <Nav.Link href='/admin/manage-users'>Manage Users</Nav.Link>
@@ -25,10 +69,10 @@ const NavBar = () => {
                     <Nav.Link href='/map'>Map View</Nav.Link>
                     <Nav.Link href='/profile'>Profile</Nav.Link>
                     <Nav.Link href='/cart'>Cart</Nav.Link>
-                    </Nav>}
-            </Container>
-        </Navbar>
-    )
+                    </Nav>} */}
+      </Container>
+    </Navbar>
+  );
 };
 
 export default NavBar;
