f921d16e3e7e5f617f8a4c213e82671c8cfad82c
msiva06 <m.sivasubramanian06@gmail.com>
2023-03-26 21:51:12 -0400

    added loading state in private routers

--- src/components/Auth/PrivateAdminRoute.js
+++ src/components/Auth/PrivateAdminRoute.js
@@ -3,9 +3,9 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "../../contexts/AuthContext";
 
 export default function PrivateAdminRoute({ children }) {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
   console.log("User in Admin router:", user);
-  if (!user || !user?.isAdmin || user?.isRestaurantOwner) {
+  if ((!user || !user?.isAdmin || user?.isRestaurantOwner) && !loading) {
     return <Navigate to="/authorizationerror" replace />;
   }
   return children;
