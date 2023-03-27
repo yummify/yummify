e45a77fffdc4aec3ec7fabb541ba7adf1eda668e
slightdevastation <jacqueline.basanta@gmail.com>
2023-03-27 07:36:20 -0500

    added auth code. UNTESTED

--- src/components/Restaurant/RestaurantStore.js
+++ src/components/Restaurant/RestaurantStore.js
@@ -1,6 +1,9 @@
 import React, {useState, useEffect} from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {auth} from "../../firebase/config";
+import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { fetchUserAsync, selectUser } from "../User/userSlice";
 
 import { fetchRestaurantAsync, selectRestaurant } from "./restaurantSlice";
 import { useAuth } from "../../contexts/AuthContext";
@@ -14,29 +17,47 @@ import { ListGroup } from "react-bootstrap";
 import AddBagForm from "../Bag/AddBagForm";
 import EditBagForm from "../Bag/EditBagForm";
 
+
+//TODO: auth is incomplete,
+//      thunk: fetchGroupBagByRestAsync is not working as intented,
+//      thunk: legacy fetchSingleBagByRestAsync still remains
+
 const RestaurantStore = () => {
-
+    /* // for testing only: edit AddBagForm to reflect correct restID variable
     const testrest = "1iPuXMXLQiICpJ8zBhiB"
-    /* const authRestaurant = useSelector(selectRestaurant);
-    
-    console.log("authrestaurant: ", authRestaurant);
-    const { user } = useAuth();
-    const dispatch = useDispatch();
-
     useEffect(()=>{
-        if(user?.userId)dispatch(fetchRestaurantAsync(user?.userId));
-    },[dispatch, user]); */
+        dispatch(fetchGroupBagByRestAsync(testrest));
+    },[dispatch]); */
+
+    //auth code: incomplete --- testing required.
+    const authRestaurant = useSelector(selectRestaurant);
+    console.log("authrestaurant:", authRestaurant);
+    const { user } = useAuth();
+    const navigate = useNavigate();
     const dispatch = useDispatch();
+    
+    useEffect(() => {
+      if (user?.userId) dispatch(fetchGroupBagByRestAsync(user?.userId));
+    }, [dispatch, user?.userId]);
+  
+    const logout = async () => {
+      try {
+        await signOut(auth);
+        navigate("/");
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    
+
+    const restaurantId = user?.userId;
     const bags = useSelector(selectBag);
 
-
+    //to sort bags from array from active/inactive
     const checkActive = (expir, quant) =>{
-        
         const parts = expir.split('-');
         const expdate = new Date(parts[0], parts[1]-1, parts[2]);
-        
         const today = new Date();
-   
         if(expdate.getTime() >= today.getTime() && quant > 0){
            return true;
         }
@@ -46,18 +67,16 @@ const RestaurantStore = () => {
          
     }
 
-
-    useEffect(()=>{
-        dispatch(fetchGroupBagByRestAsync(testrest));
-    },[dispatch]);
+    
 
 return(
     <Card>
         <Card.Title>Restaurant - Owner View</Card.Title>
         <Card.Text>
             hi. test card!
+            <Button onClick={logout}>Logout</Button>
         </Card.Text>
-        <AddBagForm restaurant={testrest}/>
+        <AddBagForm restaurant={restaurantId}/>
 
         <Card style={{backgroundColor: "lightblue"}}>
             <Card.Title>Active Bags</Card.Title>
