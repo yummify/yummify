72dd020cfd21d1786319d7611629f48339c04405
msiva06 <m.sivasubramanian06@gmail.com>
2023-03-31 16:07:55 -0400

    added styling to authorization error

--- src/components/Auth/AuthorizationError.js
+++ src/components/Auth/AuthorizationError.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { auth } from "../../firebase/config";
 import { signOut } from "firebase/auth";
 import { Link } from "react-router-dom";
+import { Container } from "react-bootstrap";
 const AuthorizationError = () => {
   useEffect(() => {
     const logout = async () => await signOut(auth);
@@ -9,8 +10,10 @@ const AuthorizationError = () => {
   }, []);
   return (
     <div>
-      <h1>You are not authorized to view this page</h1>
-      <Link to={"/"}>Return to Home</Link>
+    <Container>
+      <h1 className="mx-3">You are not authorized to view this page</h1>
+      <Link to={"/"} className="mx-3">Return to Home</Link>
+      </Container>
     </div>
   );
 };
