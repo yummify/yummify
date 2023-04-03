69e56c973230ca48509443a1a3e9a6e4bb821e26
msiva06 <m.sivasubramanian06@gmail.com>
2023-04-02 22:33:08 -0400

    removed unwanted imports and console logs in App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,28 +4,15 @@ import { AuthProvider } from "../../contexts/AuthContext";
 import Splash from "../SplashPage/SplashPage";
 import AppRoutes from "./AppRoutes";
 import NavBar from "../NavBar/NavBar";
-import { useNavigate, useLocation } from "react-router-dom";
-import { Button } from "react-bootstrap";
+import {useLocation } from "react-router-dom";
 import Footer from "../Footer/Footer";
-import { useAuth } from "../../contexts/AuthContext";
+
 
 function App() {
-  const user = useAuth();
-  console.log(user);
   const location = useLocation();
   // location.pathname(/)
   return (
     <div className="App">
-      {/* <Link to={"/"} className="mx-3">
-        Home
-      </Link>
-      <Link to={"/userstart"} className="mx-3">
-        User
-      </Link>
-      <Link to={"/restaurantstart"} className="mx-3">
-        Restaurant
-      </Link> */}
-
       <AuthProvider>
         {location.pathname === "/" && <Splash/>}
         {location.pathname !== "/" && <NavBar/>}
