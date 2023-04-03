f04a4a10bb8d0f9df8036f77e71218e8ef8d04cc
megtb <megbickerstaff@gmail.com>
2023-04-03 17:09:18 -0400

    Adding info that this is capstone

--- src/components/Footer/About.js
+++ src/components/Footer/About.js
@@ -5,6 +5,9 @@ function About() {
     <div className="about-container">
       <h1>About Yummify</h1>
 
+      <p>Yummify is a capstone project for the Grace Hopper program at FullStack Academy.
+      </p>
+
       <p>The Food & Drug Administration estimates that 30-40% of the US’s food supply is wasted.  Sending food to landfills not only contributes to heat-trapping greenhouse gasses, it also wastes the water, energy, and human labor that went into creating it.  
       </p>
       <p>
@@ -13,7 +16,7 @@ function About() {
       <p>
       Yummify helps businesses by supporting them in reducing food waste.  This platform allows restaurants to offer discount “surprise bags,” at the hours of their choosing, so that their unsold or unused food goes to customers, rather than into the trash.  They reduce the amount of trash hauling they need, recoup some of the costs for purchasing ingredients and preparing the food, and demonstrate to their customers that they are taking responsibility for their environmental impact.  
       </p>
-      
+
     </div>
   );
 }
